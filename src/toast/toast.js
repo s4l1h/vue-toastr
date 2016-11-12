eb225b5cf712fb8140abde39927e6f742004d1f1
Ryan Drost <ryan.s.drost@gmail.com>
2016-11-12 14:51:35 -0600

    onHover reset the timeout

--- src/toast/toast.js
+++ src/toast/toast.js
@@ -8,12 +8,28 @@ export default {
     created() {
         //console.log("created", this.data);
         if (typeof this.data.timeout != "undefined" && this.data.timeout != 0) {
-            setTimeout(function() {
-                this.close();
-            }.bind(this), this.data.timeout);
+            this.setTimeout();
         }
     },
     methods: {
+        // Enter Hover
+        onMouseOver() {
+            if(!this.data.closeOnHover){
+                clearInterval(this.data.intervalId);
+            }
+        },
+        // Leave Hover 
+        onMouseOut() {
+            if(!this.data.closeOnHover){            
+                this.setTimeout();
+            }
+        },
+        // Set timeout to close
+        setTimeout() {
+            this.data.intervalId = setTimeout(function() {
+                this.close();
+            }.bind(this), this.data.timeout);
+        },
         // Clicked Toast
         clicked() {
             if (typeof this.data.onClicked != "undefined") {
